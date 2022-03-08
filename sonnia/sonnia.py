17430630d5cd9e40b0646154e3e9e0296ad18b8a
giulioisac <giulioisac@gmail.com>
2022-03-08 17:40:25 +0100

    update

--- sonnia/sonnia.py
+++ sonnia/sonnia.py
@@ -244,9 +244,9 @@ class SoNNia(Sonia):
 
     def _encode_data(self,seq_features):
         length_input=len(self.features)
-        data=np.array(seq_features,dtype=object)
-        data_enc = np.zeros((len(data), length_input), dtype=np.int8)
-        for i in range(len(data_enc)): data_enc[i][data[i]] = 1
+        #data=np.array(seq_features,dtype=object)
+        data_enc = np.zeros((len(seq_features), length_input), dtype=np.int8)
+        for i in range(len(data_enc)): data_enc[i][seq_features[i]] = 1
         enc1,enc2,enc3=[],[],[]
         for x in data_enc:
             enc1.append(x[:self.l_length])
